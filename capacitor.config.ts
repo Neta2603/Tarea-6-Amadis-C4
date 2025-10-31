cd37406efdc2bec993cdc33fd0d7578e14a92d7a
Neta2603 <enlg2603@gmail.com>
2025-10-30 20:17:29 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Couteau',
+  webDir: 'dist'
+};
+
+export default config;
